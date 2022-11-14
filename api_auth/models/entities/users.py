aeffe9056ac74bfc0ae9dcac7f2d37508c347eb3
Sinester <miguelcenices13@gmail.com>
2022-11-14 18:56:58 -0400

    Finish Write token and Validate token

--- api_auth/models/entities/users.py
+++ api_auth/models/entities/users.py
@@ -3,17 +3,20 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+
 class User(Base):
     __tablename__ = config('TABLE_NAME')
     id = Column(Integer(), primary_key=True)
     email = Column(String(), nullable=False, unique=True)
     password = Column(String(), nullable=False)
+
     def __str__(self):
         return self.email
+
 
 class ModelsUsers:
     def __init__(self, id, email, password) -> None:
         self.id = id
         self.email = email
         self.password = password
-
